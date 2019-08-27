670aee6286cf087971aec99239783c8427f87354
Farahnaz Ahmed <54003741+farahnaz-a@users.noreply.github.com>
2019-08-27 17:10:49 +0600

    Portfolio Upload

---
+++ Bizzency/js/script.js
@@ -0,0 +1,9 @@
+$(".fa-bars").click(function(){
+    $(".menu").fadeToggle();
+});
+$(".fa-search").click(function(){
+    $(".over").fadeIn();
+});
+$(".close").click(function(){
+    $(".over").fadeOut();
+});
